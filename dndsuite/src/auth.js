86b0583abc9a6b607993018b0021b946f009ef6e
zuedev <zuedev@gmail.com>
2024-04-22 07:38:06 +0100

    get guilds and guilds.join scopes in discord auth

--- dndsuite/src/auth.js
+++ dndsuite/src/auth.js
@@ -2,5 +2,13 @@ import NextAuth from "next-auth";
 import Discord from "next-auth/providers/discord";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Discord],
+  providers: [
+    Discord({
+      clientId: process.env.AUTH_DISCORD_ID,
+      clientSecret: process.env.AUTH_DISCORD_SECRET,
+      authorization:
+        "https://discord.com/api/oauth2/authorize?scope=" +
+        encodeURIComponent("identify email guilds guilds.join"),
+    }),
+  ],
 });
